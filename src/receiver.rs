0c0ec4ad179078d2c56d7c55f9fa2e048ee0e3cd
Snehaan <146335670+SnehaanKandaswamy@users.noreply.github.com>
2026-07-04 23:20:04 +0530

    time

--- src/receiver.rs
+++ src/receiver.rs
@@ -273,7 +273,7 @@ pub fn run() -> Result<()> {
     use std::time::Duration;
 
 udp.set_read_timeout(
-    Some(Duration::from_millis(1))
+    Some(Duration::from_millis(100))
 )?;
     use socket2::Socket;
 
