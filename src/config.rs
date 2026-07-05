1eff0432f99b87e05bd05a9be2e86d277f3df821
Snehaan <146335670+SnehaanKandaswamy@users.noreply.github.com>
2026-07-05 10:23:48 +0530

    chunk_size 16kb

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ pub const DATA_PORT: u16 = 5000;
 
 pub const KEY_PORT: u16 = 5001;
 
-pub const CHUNK_SIZE: usize = 65000;
+pub const CHUNK_SIZE: usize = 16384;
 
 pub const NUM_WORKERS: usize = 4;
 
