483625857a5b3038f6616f15401f03f1af5e8ae5
Snehaan <146335670+SnehaanKandaswamy@users.noreply.github.com>
2026-07-07 13:33:25 +0530

    more debug finals

--- src/receiver.rs
+++ src/receiver.rs
@@ -449,8 +449,11 @@ pub fn run() -> Result<()> {
     use socket2::Socket;
 
     let socket = Socket::from(udp.try_clone()?);
-    socket.set_recv_buffer_size(64 * 1024 * 1024)?;
-
+    socket.set_recv_buffer_size(UDP_RECV_BUFFER)?;
+    println!(
+    "Actual UDP recv buffer = {} bytes",
+    socket.recv_buffer_size()?
+);
     println!("Receiver bound to {}", udp.local_addr()?);
     println!("Waiting for UDP...");
     println!("Receiver UDP: {}", udp.local_addr()?);
