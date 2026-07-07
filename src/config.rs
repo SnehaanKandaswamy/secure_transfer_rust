dc68db3057d4567b4edd1f75872f50302cfe3198
Snehaan <146335670+SnehaanKandaswamy@users.noreply.github.com>
2026-07-07 07:57:27 +0530

    x

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 //BEST
 pub const HOST: &str = "0.0.0.0";
-pub const RECEIVER_IP: &str = "192.168.1.157";
+pub const RECEIVER_IP: &str = "192.168.31.120";
 pub const DATA_PORT: u16 = 5000;
 
 pub const KEY_PORT: u16 = 5001;
